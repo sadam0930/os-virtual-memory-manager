2e66712a51dd66a2ebbb7c3b64ef512861b630ca
sadam0930 <s.adam0930@gmail.com>
2016-07-03 14:49:50 -0400

    start capturing things

--- mmu.cpp
+++ mmu.cpp
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <string>
 
+#include "pager.h"
+
 using namespace std;
 
 //begin memory management unit simulation
 int main(int argc, char **argv){
 	int opt;
-	
+	while ((opt = getopt (argc, argv, "vs:")) != -1) {
+		//instantiate a scheduler based on the -s option
+        if (opt == 'a') {}
+        if (opt == 'o') {}
+        if (opt == 'f') {}
+    }
+
+	string filename = argv[optind];
+	string randfile = argv[optind+1];
 
 	return 0;
 }
