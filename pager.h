7ee21b67a0652178bbd36312d669a80fe6f7bbde
sadam0930 <s.adam0930@gmail.com>
2016-07-06 23:28:58 -0400

    let's get this rolling

--- pager.h
+++ pager.h
@@ -7,19 +7,25 @@
 #define nullptr __null
 #endif
 
+//implementation will differ based on the scheduling algorithm
 class Pager {
 	public:
 		Pager();
 		virtual int allocate_frame(vector<PageTableEntry> * pageTable, vector<unsigned int> * frameTable, vector<unsigned int> * framesInMemory); 
 };
 
+/**************************************************************
+differentiate how to how frames are allocated between algorithms
+***************************************************************/
+
+//First Come First Serve
 class FIFO_Pager : public Pager {
 	public:
 		FIFO_Pager();
 
 		int allocate_frame(vector<PageTableEntry> * pageTable, vector<unsigned int> * frameTable, vector<unsigned int> * framesInMemory) {
 			int frameNum = -1;
-			
+
 			return frameNum;
 		}
 };
